85eb6d53c5ce0d856294ea509c1e4e9721ae8549
Alec Haag <alechaag03@gmail.com>
2025-02-18 14:47:51 -0500

    got test_ext_api_interface to 100

--- tests/test_ext_api_interface.py
+++ tests/test_ext_api_interface.py
@@ -27,6 +27,43 @@ class TestExtApiInterface(unittest.TestCase):
         requests.get = Mock(return_value=Mock(status_code=100))
         self.assertEqual(self.api.make_request(""), None)
 
+    def test_is_book_available_true(self):
+        attr = {'json.return_value': dict()}
+        requests.get = Mock(return_value = Mock(status_code=200, **attr))
+        self.api.make_request = Mock(return_value=self.json_data)
+        self.assertEqual(self.api.is_book_available(self.book), True)
+
+    def test_is_book_available_false(self):
+        attr = {'json.return_value': dict()}
+        requests.get = Mock(return_value = Mock(status_code=200, **attr))
+        self.assertEqual(self.api.is_book_available(self.book), False)
+
+    def test_get_books_by_author(self):
+        attr = {'json.return_value': dict()}
+        requests.get = Mock(return_value = Mock(status_code=200, **attr))
+        self.api.make_request = Mock(return_value=self.json_data)
+        books = self.api.books_by_author("Mark Lutz")
+        self.assertEqual(books[0].lower(), self.book)
+
+    def test_get_books_by_author_no_json_data(self):
+        requests.get = Mock(return_value=Mock(status_code=400))
+        self.assertEqual(self.api.books_by_author("Mark Lutz"), [])
+
+    def test_get_book_info(self):
+        attr = {'json.return_value': dict()}
+        requests.get = Mock(return_value=Mock(status_code=200, **attr))
+        self.api.make_request = Mock(return_value=self.json_data)
+        book_info = self.api.get_book_info(self.book)
+        self.assertEqual(book_info[0]['title'].lower(), self.book)
+
+    def test_get_book_info_no_json_data(self):
+        requests.get = Mock(return_value=Mock(status_code=400))
+        self.assertEqual(self.api.get_book_info(self.book), [])
+
     def test_get_ebooks(self):
         self.api.make_request = Mock(return_value=self.json_data)
         self.assertEqual(self.api.get_ebooks(self.book), self.books_data)
+
+    def test_get_ebooks_no_json_data(self):
+        requests.get = Mock(return_value=Mock(status_code=400))
+        self.assertEqual(self.api.get_ebooks(self.book), [])
